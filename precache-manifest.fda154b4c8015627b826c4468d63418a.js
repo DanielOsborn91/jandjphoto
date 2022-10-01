60fedfb2c4e18f5c2b368c8e7460aa931250fd92
danielosborn91 <danielosborn91@gmail.com>
2022-09-30 22:56:55 -0400

    Updates

---
+++ precache-manifest.fda154b4c8015627b826c4468d63418a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac9280416dc3ed39a1767631764f87f",
+    "url": "/jandjphoto/index.html"
+  },
+  {
+    "revision": "1b17a2f0e9d6b599c2fe",
+    "url": "/jandjphoto/static/css/2.c9d8cff1.chunk.css"
+  },
+  {
+    "revision": "f4755464890bb979b535",
+    "url": "/jandjphoto/static/css/main.6bb29406.chunk.css"
+  },
+  {
+    "revision": "1b17a2f0e9d6b599c2fe",
+    "url": "/jandjphoto/static/js/2.85c9dd78.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/jandjphoto/static/js/2.85c9dd78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4755464890bb979b535",
+    "url": "/jandjphoto/static/js/main.dc4da5e1.chunk.js"
+  },
+  {
+    "revision": "a5e417f9d4c67c6f3ed6",
+    "url": "/jandjphoto/static/js/runtime-main.7a33179a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/jandjphoto/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/jandjphoto/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/jandjphoto/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/jandjphoto/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/jandjphoto/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "02bba601716f9a0ff1e241a64cfe9b29",
+    "url": "/jandjphoto/static/media/lobster-latin-400.02bba601.woff"
+  },
+  {
+    "revision": "fe012fb7b22e5776c4a2e91889068974",
+    "url": "/jandjphoto/static/media/lobster-latin-400.fe012fb7.woff2"
+  }
+]);
